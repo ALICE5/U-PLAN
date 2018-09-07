6ff5ec0c9df3ac8b2869d95abf0deabc7c7ae20e
Yan <alice4u@bjtu.edu.cn>
2018-09-07 23:43:06 +0800

    0905

--- 0905/term.py
+++ 0905/term.py
@@ -7,16 +7,16 @@ url = "https://mp.weixin.qq.com/cgi-bin/appmsg"
 
 # 使用Cookie，跳过登陆操作
 headers = {
-  "Cookie": "RK=v8HHn32beS; o_cookie=1152757217; tvfe_boss_uuid=48c68ff8346fed5a; pgv_pvi=482764800; pgv_pvid=5843832048; sd_userid=44481512025843983; sd_cookie_crttime=1512025843983; ptcz=09cba92f0b66dc4110ebbd50e8b13b2f08fa5f1bf53a553ca5af72dc8079eaa9; pt2gguin=o1152757217; pac_uid=1_1152757217; pgv_si=s7155075072; rewardsn=; wxtokenkey=777; pgv_info=ssid=s4382189760; ptisp=cnc; qqmusic_uin=; qqmusic_key=; qqmusic_fromtag=; rv2=80EAF94F00D8AB2BD115516CA7451F9E6BAD07D03CE2BD2BAB; property20=9E5CAC6585A719CF48D47FEEF8DFC53680C69CF81CB0E167DAD296917F48D75EB90E2AA701986CD4; uin=o1152757217; ua_id=wOPSwuAKtjxWkL1ZAAAAALKA-hHduzRvcqIH00rfLdY=; mm_lang=zh_CN; cert=wM3KsfbjD2oKcag_gee3cPc3sPrYLLOg; skey=@Z8rC5TbqN; sig=h0171efcca48d698264d02bd2f29736c56d99ad389a5144e779a1edaf440efcf3a84da2ba7f61864b81; noticeLoginFlag=1; uuid=1d112f9695d31d4bc84f7e4a42eceaea; ticket=8983aeac3d720526dd11be54cddfe1bb3e221ff2; ticket_id=gh_4b15d0f326ca; data_bizuin=3269626445; bizuin=3261626562; data_ticket=/KgKUeFte0ItNYCDa4IFH55H8YuuUTOcCBl+s49zUiKYlYD/SoJTtm71Us3drt6E; slave_sid=RldzV1M1aUpocmNzTjFtajllSUVvRkRZUTRldzE4VEpsTFJuZnp3VWJGZkVLVm1FaEFER1BIdWJRRnpwZnJXSzFjV2pGbXVJaUlTMHhCb3pNS2tZNlM2MWhxTVZfQXdXNjVMcW5oTHN6enk5MWVUTUJ3RVJVRHp6SWZWb3NJRUVCSnFUTTBHSDJPOHdQUUxh; slave_user=gh_4b15d0f326ca; xid=f23f9536aeed400f6c205cdfa3394fe0; openid2ticket_oTYriwurAE1152tR1qdH2TZuKdsk=10HA6KpX5JUsqNbJ+pilxz/Q3LMbh52yd2JcJtV5vEk=",
+  "Cookie": "noticeLoginFlag=1; RK=v8HHn32beS; o_cookie=1152757217; tvfe_boss_uuid=48c68ff8346fed5a; pgv_pvi=482764800; pgv_pvid=5843832048; sd_userid=44481512025843983; sd_cookie_crttime=1512025843983; ptcz=09cba92f0b66dc4110ebbd50e8b13b2f08fa5f1bf53a553ca5af72dc8079eaa9; pac_uid=1_1152757217; pgv_si=s7155075072; rewardsn=; wxtokenkey=777; pgv_info=ssid=s4382189760; ptisp=cnc; qqmusic_uin=; qqmusic_key=; qqmusic_fromtag=; rv2=80EAF94F00D8AB2BD115516CA7451F9E6BAD07D03CE2BD2BAB; property20=9E5CAC6585A719CF48D47FEEF8DFC53680C69CF81CB0E167DAD296917F48D75EB90E2AA701986CD4; ua_id=wOPSwuAKtjxWkL1ZAAAAALKA-hHduzRvcqIH00rfLdY=; mm_lang=zh_CN; cert=wM3KsfbjD2oKcag_gee3cPc3sPrYLLOg; noticeLoginFlag=1; ptui_loginuin=774166816@qq.com; pt2gguin=o0774166816; uin=o0774166816; skey=@dZ9OahheV; sig=h01bbf451bd100ae3be7ddf3f7f28df4fda3ac33ac292bc99cf78e42592e7821175c6ccaa5e9ba14c39; uuid=0b63ea3ac31688b43e09f4ba370ae3cb; ticket=a4f6f86fb3b91f64018bb72231685e8e818267a1; ticket_id=gh_be0398706d07; data_bizuin=3076321522; bizuin=3007795348; data_ticket=YICX/LKc6HfLAAP7sRzBqd3fg9RzKMKbkhwVBj9NN15RfkEBmK1PaABeohWcxh5l; slave_sid=WHNmNVZZS3hFdFZ6dEFhY0VPRWVHZ3ZEVUFjOVExUG0zWGM3bTNxZVZlekZnT0RoYk1KWVhLeTR1b2Noam5IQ3hSdTJGYlg0YzZQU1BaeU1mTldFaXF3cFhDOWljMl9UYWpXUUdWUnU5dVpraVZnSjhXTVE3a2UzUkQwTFBjSGZVT285ekNsNlNpdVlkZkNR; slave_user=gh_be0398706d07; xid=a1c1d7e8f344b666a72348631e3deb23; openid2ticket_o8vBct9--zg6JOzQ-CK6HLgkjxGE=p9kQOU/Dvs9+DcdGQ1Yx5pW+lvqO0MX/mriwkgN82hs=",
   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
 }
 
 data = {
-    "token": "673840951",
+    "token": "1969523378",
     "lang": "zh_CN",
     "f": "json",
     "ajax": "1",
-    "random": "0.5152365215315493",
+    "random": "0.5266656784134875",
     "action": "list_ex",
     "begin": "0",
     "count": "5",
@@ -26,7 +26,7 @@ data = {
 }
 
 
-for page in range(74,80):
+for page in range(85,101):
     print("第" + str(page) + "页")
     data["begin"] = page * 5
     content_json = requests.get(url, headers=headers, params=data).json()
@@ -54,14 +54,14 @@ for page in range(74,80):
         "Origin": "https://mp.weixin.qq.com",
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15G77 MicroMessenger/6.7.2 NetType/WIFI Language/zh_CN",
         "Connection": "keep-alive",
-        "Referer": "https://mp.weixin.qq.com/mp/getappmsgext?f=json&mock=&uin=777&key=777&pass_ticket=ZcXAq5mTR2z3xwme2uP7cMk3k7EXbdtj3dmcz7OcCNQSkzZHiz66WHxPYHDCct3q&wxtoken=777&devicetype=iOS11.4.1&clientversion=16070228&appmsg_token=973_r%252Fs%252FmrXQOeD%252Fd8xJbuY19LQbyofX3CnRgiGUzMMaYCf1eOMIaNp-WpVPaRIPNbPecwjvxGIhVqV-y3ni&x5=0&f=json",
-        "Cookie": "devicetype=iOS11.4.1; lang=zh_CN; pass_ticket=ZcXAq5mTR2z3xwme2uP7cMk3k7EXbdtj3dmcz7OcCNQSkzZHiz66WHxPYHDCct3q; rewardsn=; version=16070228; wap_sid2=CJDO3L0MElw3UDhDd3RURkJndmp6Z2l0RlJfem1hRXJ1ZnUycE9pLXhJcmNMb3d4ZEt0c1RFT21VdWRRQUktcnN4MGZMQTAtMzg5RndHU01KLTFRTzVBSDR4M21wYzBEQUFBfjD6h8ncBTgNQAE=; wxtokenkey=777; wxuin=3350669072"
+        "Referer": "https://mp.weixin.qq.com/s?__biz=MzA4NDI3NjcyNA==&mid=2649409055&idx=2&sn=c0bc2c942b3b801ffe2cc9dc639e1a0b&chksm=87f7c284b0804b92e9258fec2f518a14651436c9a4b0231c057d05c445db1c6e71d2f61cfbd8&scene=126&ascene=0&devicetype=iOS11.4.1&version=16070228&nettype=WIFI&abtest_cookie=BQABAAgACgALABIAEwAFAJ%2BGHgAjlx4AT5keAFeZHgBomR4AAAA%3D&lang=zh_CN&fontScale=100&pass_ticket=pY%2FCQMeGGX4sq6cmVuKov3oLFEz7f%2FHWOidYOF11wlwa%2FB9K4dceIfg8O1wF15Hj&wx_header=1",
+        "Cookie": "devicetype=iOS11.4.1; lang=zh_CN; pass_ticket=pY/CQMeGGX4sq6cmVuKov3oLFEz7f/HWOidYOF11wlwa/B9K4dceIfg8O1wF15Hj; rewardsn=; version=16070228; wap_sid2=CJDO3L0MElx2UGxUQlB1cmJIR3d0Z0htY3d3b2lGeHVsemZyV0dCWjY2aUNBcU41SzJ3ME5rT2N1VXo1QWNyUUV6UDY1WHBKSWh3TEdKd2l2NFZxUWs3V0JCR2NyTTBEQUFBfjDY+sncBTgNQAE=; wxtokenkey=777; wxuin=3350669072"
         }
 
         data1 = {
             "is_only_read": "1",
-            "req_id": "0717khT1SH5AlIZRzJwy1Hxx",
-            "pass_ticket": "ZcXAq5mTR2z3xwme2uP7cMk3k7EXbdtj3dmcz7OcCNQSkzZHiz66WHxPYHDCct3q",
+            "req_id": "0721zRj3WnHPr9OXxGiflAcd",
+            "pass_ticket": "pY%252FCQMeGGX4sq6cmVuKov3oLFEz7f%252FHWOidYOF11wlwa%252FB9K4dceIfg8O1wF15Hj",
             "is_temp_url": "0",
         }
 
@@ -71,8 +71,8 @@ for page in range(74,80):
             "sn": sn,
             "idx": idx,
             "key": "777",
-            "pass_ticket": "ZcXAq5mTR2z3xwme2uP7cMk3k7EXbdtj3dmcz7OcCNQSkzZHiz66WHxPYHDCct3q",
-            "appmsg_token": "973_4OcBhnoSfbDE36r4zsxBGfRBKGhgacCDet_MWZfNJLF6x2tF9LIi9qdb5I-SuA-Y4hqQpzpvv82eRZ6i",
+            "pass_ticket": "pY%252FCQMeGGX4sq6cmVuKov3oLFEz7f%252FHWOidYOF11wlwa%252FB9K4dceIfg8O1wF15Hj",
+            "appmsg_token": "973_9ZU4XlMiQMlb7kJbtJ_YKMu3ujc-THWkrujXpSDmhb2IgH1J-DemV9OCR9ul6ERPugb6gdgPQFAVHfMD",
         }
 
         url1 = "http://mp.weixin.qq.com/mp/getappmsgext"
--- 0906/author.py
+++ 0906/author.py
@@ -12,7 +12,8 @@ Content-Type: application/x-www-form-urlencoded; charset=UTF-8
 Origin: https://mp.weixin.qq.com
 User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15G77 MicroMessenger/6.7.2 NetType/WIFI Language/zh_CN
 Connection: keep-alive
-Cookie: devicetype=iOS11.4.1; lang=zh_CN; pass_ticket=B2a5rvISwTcdJf2822bRTSAdJVD/0SDHlew4fYsbIcAiLOYLDwWS6CKQ8kLamgWd; rewardsn=; version=16070228; wap_sid2=CJDO3L0MEogBaDJUUmlXR1lJRURrNmg4c0tvUWNQaXRMOHlxS25oMndGdWdOdk03dHdYbHVaa0RzeHJQcFBXSl9QRnNyRUgwN1hnUklOTndxeGNGd2ZwRVFqUEZQbEd4bWtFUV9KUGZEcGdUQzdLNXpYYnlXVDNvanhFNUpIemhFZU1nemhHWGp6UU1BQUF+fjDV1MXcBTgNQAE=; wxtokenkey=777; wxuin=3350669072
+Referer: https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzA4NDI3NjcyNA==&scene=126&devicetype=iOS11.4.1&version=16070228&lang=zh_CN&nettype=WIFI&a8scene=0&fontScale=100&pass_ticket=JlWvupzG2v%2Fc%2FL%2B2RIGSsJXmN%2B7sgkfLiTfvHqHov0DWUMxT5ndkkcLiXS4BTuWl&wx_header=1
+Cookie: devicetype=iOS11.4.1; lang=zh_CN; pass_ticket=JlWvupzG2v/c/L+2RIGSsJXmN+7sgkfLiTfvHqHov0DWUMxT5ndkkcLiXS4BTuWl; version=16070228; wap_sid2=CJDO3L0MElxWS0lfYzc1b19OU3RxRUtZTmRiSkgxMFB2Z1ljNHVVVEJwazJ3ZHpIcHVyU1dlZUJzTmxmTVhkakczMmhlZmxjUjEyTUUyel8xZHRZaUY1c09QNktZczBEQUFBfjDypsrcBTgNQJVO; wxuin=3350669072; wxtokenkey=777; rewardsn=
 '''
 
 headers = headers.split("\n")
@@ -22,8 +23,19 @@ for h in headers:
         k, v = h.split(":", 1)
         d_headers[k] = v.strip()
 
+url = "https://mp.weixin.qq.com/mp/profile_ext?action=getmsg" \
+      "&__biz=MzA4NDI3NjcyNA==" \
+      "&f=json" \
+      "&offset=11" \
+      "&count=10" \
+      "&is_ok=1" \
+      "&scene=126" \
+      "&uin=777" \
+      "&key=777" \
+      "&pass_ticket=JlWvupzG2v%2Fc%2FL%2B2RIGSsJXmN%2B7sgkfLiTfvHqHov0DWUMxT5ndkkcLiXS4BTuWl" \
+      "&wxtoken=&appmsg_token=973_wy37kQhuGQU%252FedEiT_jTwMJclqrrZw34ivNSVA~~&x5=0" \
+      "&f=json"
 
-url = "https://mp.weixin.qq.com/mp/getappmsgext"
 data = {
     "is_only_read":"1",
     "req_id":"0701mMJ52qbzeA1jjnp6Ka88",
@@ -42,7 +54,7 @@ params = {
 }
 
 
-response = requests.post(url, headers=d_headers, data=data, params=params)
+response = requests.get(url, headers=d_headers)
 print(response.json())
 
 
