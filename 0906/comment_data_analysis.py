dac393a36509c066839ddf33e3a9c40e66df68aa
Yan <alice4u@bjtu.edu.cn>
2018-09-11 17:04:48 +0800

    0911

--- 0906/comment_data_analysis.py
+++ 0906/comment_data_analysis.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import pandas as pd
 from pandas import DataFrame
 import jieba.analyse
@@ -6,6 +7,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import numpy as np
+from pylab import *
+import random
+
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname="/Users/alice/Desktop/simhei.ttf",size=10)
 
 # datafile_path = '/Users/alice/Desktop/XinHuaShe_Comment_All.csv'
 # data_df = pd.read_csv(datafile_path)
@@ -31,14 +37,21 @@ with open('./XinHuaShe_Comment_Cut.csv',encoding='utf-8',errors='ignore') as csv
     str_convert = ''.join([a for a in str_convert if a not in punct])
     # print(str_convert)
 
-    content = jieba.analyse.extract_tags(str_convert, topK=1000, withWeight=True,
+    content = jieba.analyse.extract_tags(str_convert, topK=100, withWeight=True,
                                          allowPOS=('ns', 'n', 'vn', 'nr', 'nrfg'))
-    tf = dict((a[0], a[1]) for a in content)
+    print(content)
+    random.shuffle(content)
+    print(content)
+    # tf = dict((a[0], a[1]) for a in content)
     # print(tf)
-    sort_tf = sorted(tf.items(), key=lambda x: x[1], reverse=True)
-    sort_word_top100 = [i[0] for i in sort_tf][0:100]
+    # sort_tf = sorted(tf.items(), key=lambda x: x[1], reverse=True)
+    # sort_word_top100 = [i[0] for i in sort_tf][0:100]
+    # print(sort_word_top100)
+    # sort_freq_top100 = [i[1] for i in sort_tf][0:100]
+    # print(sort_freq_top100)
+    sort_word_top100 = [i[0] for i in content]
+    sort_freq_top100 = [i[1] for i in content]
     print(sort_word_top100)
-    sort_freq_top100 = [i[1] for i in sort_tf][0:100]
     print(sort_freq_top100)
 
     heatmap = np.array(sort_freq_top100).reshape((10, 10))
@@ -48,14 +61,17 @@ with open('./XinHuaShe_Comment_Cut.csv',encoding='utf-8',errors='ignore') as csv
     for i in range(10):
         for j in range(10):
             text = ax.text(j, i, heatmapwords[i, j],
-                           ha="center", va="center", color="w")
+                           ha="center", va="center", color="w", fontproperties=font)
 
-    ax.set_title("heat of words")
     fig.tight_layout()
-    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    # plt.title('文章评论热词TOP100', fontproperties=font)
+    # plt.legend(loc='upper right')
+    plt.colorbar(im)
+    plt.axis('off')
+
+
+    plt.savefig("./comment_hotword_100.jpeg",dpi = 600)
     plt.show()
-
-
-    # 排序
 
 
